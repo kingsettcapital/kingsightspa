e1c9a1b7540aac92805d703c49e27a702e7e4616
Suresh Kannan <SKannan@Kingsettcapital.com>
2026-06-04 16:20:38 -0400

    environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -7,7 +7,7 @@ export const environment: Environment = {
   azureConfig: {
       clientId: 'cfb0f697-501d-4f4e-a200-04187704a1af',
    authority: 'https://login.microsoftonline.com/f6d94abc-5472-43af-ab66-95726e5ab0cc/',
-    redirectURL: 'http://kingsightdev.kingsettcapital.com',
+    redirectURL: 'https://kingsightdev.kingsettcapital.com',
     postLogoutRedirectUri: 'http://kingsightdev.kingsettcapital.com/auth/login',  
     scopes: "api://444a5811-9469-445c-b83d-6f3c53328b10/Read"
   },
