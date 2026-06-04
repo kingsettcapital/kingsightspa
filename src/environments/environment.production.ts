e72e34d1a4b51c53ac7eb9fb27ba161569441103
Suresh Kannan <SKannan@Kingsettcapital.com>
2026-06-04 15:22:14 -0400

    URL

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -3,12 +3,12 @@ import type { Environment } from '../app/core/interfaces/environment.interfaces'
 export const environment: Environment = {
   production: true,
   requireLogin: true,
-  apiUrl: 'https://localhost:7140',
+  apiUrl: 'https://kingsightdevapi.kingsettcapital.com/',
   azureConfig: {
-    clientId: 'YOUR_CLIENT_ID',
-    authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID',
-    redirectURL: 'http://localhost:4200',
-    postLogoutRedirectUri: 'http://localhost:4200/auth/login',
-    scopes: 'api://YOUR_API_APP_ID/.default',
+     clientId: 'c505fbcc-5f64-4025-bae9-bccb863c4728',
+    authority: 'https://login.microsoftonline.com/f6d94abc-5472-43af-ab66-95726e5ab0cc/',
+    redirectURL: 'http://kingsightdev.kingsettcapital.com',
+    postLogoutRedirectUri: 'http://kingsightdev.kingsettcapital.com/auth/login',  
+    scopes: 'api://4092694a-0ad6-4257-8cc5-719335557535/Read',
   },
 };
