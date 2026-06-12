cd9acefdeb57c6cd36cd7c570da0c9fbc5cadd6c
lokesh75way <lokesh@75way.com>
2026-06-12 15:08:36 +0530

    feat: revert azureConfig change from enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,15 @@ import type { Environment } from '../app/core/interfaces/environment.interfaces'
 
 export const environment: Environment = {
   production: false,
-  requireLogin: false,
+  requireLogin: true,
   // apiUrl: 'https://kingsightdevapi.kingsettcapital.com/',
   apiUrl: 'https://721e-38-183-10-85.ngrok-free.app',
   mockUserRole: UserRole.Administrator,
   azureConfig: {
-      clientId: 'cfb0f697-501d-4f4e-a200-04187704a1af',
-   authority: 'https://login.microsoftonline.com/f6d94abc-5472-43af-ab66-95726e5ab0cc/',
-     redirectURL: 'https://localhost:4200',
-    postLogoutRedirectUri: 'http://kingsightdev.kingsettcapital.com/auth/login',  
-    scopes: "api://444a5811-9469-445c-b83d-6f3c53328b10/Read"
+    clientId: 'e32db1db-4cd9-4853-aa46-69cd1d63f8d7',
+    authority: 'https://login.microsoftonline.com/f6d94abc-5472-43af-ab66-95726e5ab0cc',
+    redirectURL: 'http://localhost:4200',
+    postLogoutRedirectUri: 'http://localhost:4200/auth/login',
+    scopes: 'api://367f65c5-e761-4739-897a-cff602cb119d/.default',
   },
 };
