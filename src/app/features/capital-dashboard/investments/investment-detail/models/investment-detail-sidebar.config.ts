a1354517a22144e1602e527c73b5b8c35a69d553
sgarikapati <sgarikapati@kingsettcapital.com>
2026-09-09 12:54:00 -0400

    bug fixes

--- src/app/features/capital-dashboard/investments/investment-detail/models/investment-detail-sidebar.config.ts
+++ src/app/features/capital-dashboard/investments/investment-detail/models/investment-detail-sidebar.config.ts
@@ -5,18 +5,20 @@ export const INVESTMENT_DETAIL_SIDEBAR_SECTIONS: InvestorDetailSidebarSection[] 
     title: 'Overview',
     items: [
       { id: 'overview', label: 'Overview' },
-      { id: 'fund-transactions', label: 'Transactions' },
-      { id: 'capital-account', label: 'Capital Account' },
-      { id: 'performance', label: 'Performance' },
-    ],
-  },
-  {
-    title: 'Data',
-    items: [
       { id: 'assets', label: 'Assets' },
       { id: 'financial-metrics', label: 'Financial Metrics' },
+      { id: 'fund-transactions', label: 'Transactions' },
+      { id: 'capital-account', label: 'Capital Account' },
+      // { id: 'performance', label: 'Performance' },
     ],
   },
+  // {
+  //   title: 'Data',
+  //   items: [
+  //     { id: 'assets', label: 'Assets' },
+  //     { id: 'financial-metrics', label: 'Financial Metrics' },
+  //   ],
+  // },
   // {
   //   title: 'Views',
   //   items: [
