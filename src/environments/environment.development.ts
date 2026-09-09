422829de5561531ba41b9a61f09efc0023e07ffc
sgarikapati <sgarikapati@kingsettcapital.com>
2026-09-09 17:06:11 -0400

    bug fixes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,8 +3,8 @@ import type { Environment } from '../app/core/interfaces/environment.interfaces'
 export const environment: Environment = {
   production: false,
   requireLogin: true,
-  apiUrl: 'http://localhost:7140/',
-  // apiUrl: 'https://kingsightdevapi.kingsettcapital.com/',
+  apiUrl: 'https://kingsightdevapi.kingsettcapital.com/',
+  // apiUrl: 'http://localhost:7140/',
   managementSummaryEnabled: true,
   showHomeCapitalAndDataExplorer: true,
   showAiAssistant: true,
